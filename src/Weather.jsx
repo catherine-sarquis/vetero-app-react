18edf26d8986834f1d72aa182742e07530c751df
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-21 13:50:43 +0100

    vite and react template

---
+++ src/Weather.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.city} is ${response.data.temperature.current}`
+    );
+  }
+
+  const apiURL = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=c3ae07f646b904bce9d83oat69c5764d`;
+  axios.get(apiURL).then(handleResponse);
+}
